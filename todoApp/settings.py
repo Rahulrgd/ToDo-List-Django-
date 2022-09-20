aa8446d23574971b2fe9459e1d7b831dc7c55873
RahulGupta <rahulrgd21@gmail.com>
2022-09-20 11:00:16 +0530

    step-4

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -123,7 +123,7 @@ MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # STATICFILES_DIRS = (
