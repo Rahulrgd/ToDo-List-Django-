93928de4b57322943befd83fcdda07fec2fc5d3f
RahulGupta <rahulrgd21@gmail.com>
2022-09-20 10:46:53 +0530

    modified settings static_root for heroku

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -124,6 +124,8 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'staticfiles'),
+# )
+
+STATIC_ROOT =os.join(BASE_DIR, 'static')
