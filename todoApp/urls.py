a151a38c300d61c00ad2cc37f5a05ba58ae12fb6
RahulGupta <rahulrgd21@gmail.com>
2022-09-20 11:03:37 +0530

    step-6

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -18,6 +18,9 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import settings, views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('todos/', include('todos.urls')),
