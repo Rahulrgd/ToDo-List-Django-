6dbc4056895df74869c842bfbc1bef2abc824175
RahulGupta <rahulrgd21@gmail.com>
2022-09-21 12:32:49 +0530

    setps

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8)810zj@#^2xp=1=2rkozbv8#)gub6m1a^9qf&)d-9&x9*c2a_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['todo-list-django-bist.herokuapp.com']
+ALLOWED_HOSTS = ['*', 'todo-list-django-bist.herokuapp.com']
 
 
 # Application definition
